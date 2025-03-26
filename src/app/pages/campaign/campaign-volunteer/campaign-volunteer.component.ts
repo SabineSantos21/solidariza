6138b9f4e7d14389c5d5791dc61fa7ad49cce5bd
Sabine Freiman <sabinefreiman@gmail.com>
2025-03-25 23:36:04 -0300

    KAN-14 - Telas para se inscrever para a campanha

--- src/app/pages/campaign/campaign-volunteer/campaign-volunteer.component.ts
+++ src/app/pages/campaign/campaign-volunteer/campaign-volunteer.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { CampaignService } from 'src/app/shared/services/campaign.service';
+import { CampaignVolunteerService } from 'src/app/shared/services/campaignVolunteer.service';
+import { LocalStorageService } from 'src/app/shared/services/local-storage.service';
 
 @Component({
   selector: 'app-campaign-volunteer',
@@ -6,10 +11,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./campaign-volunteer.component.scss']
 })
 export class CampaignVolunteerComponent implements OnInit {
+  volunteer: any = null;
 
-  constructor() { }
+  campaignId: any = null;
+  campaign: any = null;
+
+  created: any = null;
+
+  constructor(
+    public spinner: NgxSpinnerService,
+    public router: Router,
+    private route: ActivatedRoute,
+    public localStorageService: LocalStorageService,
+    public campaignVolunteerService: CampaignVolunteerService,
+    public campaignService: CampaignService
+  ) { }
 
   ngOnInit(): void {
+    this.campaignId = this.route.snapshot.paramMap.get("id");  
+
+    if(this.campaignId) {
+      this.volunteer = this.localStorageService.get("user");
+      this.createCampaignVolunteer();
+    }
+    else {
+      this.router.navigate(["/dashboard"]);
+    }
+  }
+
+  getCampaignById() {
+    this.spinner.show();
+
+    this.campaignService.getCampaignById(this.campaignId).subscribe(
+      data => {
+        this.campaign = data;
+      },
+      error => {
+
+      }
+    ).add(() => {
+      this.spinner.hide()
+    })
+  }
+
+  createCampaignVolunteer() {
+    this.spinner.show();
+
+    var newCampaignVolunteer = {
+      userId: this.volunteer.userId,
+      campaignId: this.campaignId
+    }
+
+    this.campaignVolunteerService.createCampaignVolunteer(newCampaignVolunteer).subscribe(
+      data => {
+        this.created = true;
+        this.getCampaignById();
+      },
+      erros => {
+        this.created = false;
+      }
+    ).add(() => {
+      this.spinner.hide();
+    })
   }
 
 }
---
+++ src/app/shared/services/campaignVolunteer.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    "Content-Type": "Application/json",
+  }),
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CampaignVolunteerService {
+
+  constructor(private http: HttpClient) { }
+  
+  getCampaignVolunteerById(campaignVolunteerId): Observable<any> {
+    return this.http.get(
+      `${environment.apiAddress}CampaignVolunteer/${campaignVolunteerId}`,
+      httpOptions
+    );
+  }
+
+  getCampaignVolunteerByCampaignId(campaignId): Observable<any> {
+    return this.http.get(
+      `${environment.apiAddress}CampaignVolunteer/Campaign/${campaignId}`,
+      httpOptions
+    );
+  }
+
+  createCampaignVolunteer(campaignVolunteer): Observable<any> {
+    return this.http.post(
+      `${environment.apiAddress}CampaignVolunteer`,
+      campaignVolunteer,
+      httpOptions
+    );
+  }
+
+  updateCampaignVolunteer(campaignVolunteerId, campaignVolunteer): Observable<any> {
+    return this.http.put(
+      `${environment.apiAddress}CampaignVolunteer/${campaignVolunteerId}`,
+      campaignVolunteer,
+      httpOptions
+    );
+  }
+
+  deleteCampaignVolunteer(campaignVolunteerId): Observable<any> {
+    return this.http.delete(
+      `${environment.apiAddress}CampaignVolunteer/${campaignVolunteerId}`,
+      httpOptions
+    );
+  }
+}
