a193f4617dfe801e2e8453997a20f3b603272fd4
Sabine Freiman <sabine.freiman@isolutions.com.br>
2025-06-04 22:23:51 -0300

    Ajustes CI-CD e Testes

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,35 +1,34 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false,
     },
+    reporters: ["progress", "kjhtml", "coverage"],
+
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage'),
-      subdir: '.',
+      dir: require("path").join(__dirname, "./coverage"),
+      subdir: ".",
       reporters: [
-        { type: 'lcovonly', subdir: '.', file: 'lcov.info' },
-        { type: 'text-summary' }
+        { type: "lcovonly", subdir: ".", file: "lcov.info" },
+        { type: "text-summary" }
       ]
     },
-    reporters: ['progress', 'kjhtml', 'coverage-istanbul'], // Add coverage-istanbul to reporters
+
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'], // Use ChromeHeadless for CI environments
-    singleRun: false,
-    restartOnFileChange: true
+    browsers: ["ChromeHeadless"],
+    singleRun: true,
+    restartOnFileChange: true,
   });
 };
